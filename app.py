0945d02fd25773bff4be475528c87b4a040e00ba
Big Vaio <bigv@localhost.localdomain>
2022-06-27 19:31:03 +0200

    Aggiunti vari commenti ad app.py e dopo aver stampato nel terminale i dati inseriti, ritorna alla pagina iniziale

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ def index():
 
 @app.route('/', methods=['POST'])
 def my_form_post():
+
+    #Prendo i dati inseriti in input dal form
     nome = request.form['nome']
-    cognome = request.form['cognome']
+    cognome = request.form['cognome'] 
     email = request.form['email']
 
-    return email
+    print(nome, cognome, email) #stampo i dati inseriti per verifica
+
+    return render_template('index.html') #ritorno alla pagina iniziale dopo aver inserito i dati
