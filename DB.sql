a5a7c364f581cbc7bd24c27470691abe19194f47
Pamagi17 <matteopeo.ma@gmail.com>
2022-06-27 17:32:14 +0200

    Added DB.sql and style.css

---
+++ DB.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE donatori;
+DROP TABLE IF EXISTS utenti;
+CREATE TABLE utenti (
+    nome varchar(20),
+    cognome varchar(20),
+    email varchar(50) PRIMARY KEY
+);
+
+INSERT INTO utenti (nome, cognome, email) VALUES (
+    'Pippo',
+    'Franco',
+    'pippo.franco@gail.com'
+
+);
--- app.py
+++ app.py
@@ -5,4 +5,8 @@ app = Flask (__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-    
+
+@app.route('/')
+def insert():
+    return 
+
