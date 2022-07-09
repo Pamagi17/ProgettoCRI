fcde9e7bf3488b6805f69c09c0f9a71eac34f62e
Andro3Grazie <andro.mieli2001@gmail.com>
2022-07-09 16:44:25 +0200

    Re-Styling form template

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask (__name__)
 
 @app.route('/')
 def index():
-    return render_template('index2.html')
+    return render_template('index.html')
 
 @app.route('/', methods=['POST'])
 def my_form_post():
 
     #Prendo i dati inseriti in input dal form
-    nome = request.form['nome']
-    cognome = request.form['cognome'] 
+    nome = request.form['firstName']
+    cognome = request.form['lastName'] 
     email = request.form['email']
 
     print(nome, cognome, email) #stampo i dati inseriti per verifica
